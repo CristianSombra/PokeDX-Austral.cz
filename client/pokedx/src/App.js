e855f210c5022db50a7aa270677be5e8d1448ecf
CristianLegion <108194989+CristianSombra@users.noreply.github.com>
2023-12-12 17:06:05 -0300

    Implementación de Ionic
    - Se agrega framework IONIC
    - Se busca compatibilidades entre las dependencias.
    - Se crea las views LANDING y HOME
    - Se realiza conexión entrelas VIEWS

--- client/pokedx/src/App.js
+++ client/pokedx/src/App.js
@@ -1,10 +1,19 @@
+import React from "react";
+import { IonApp, IonRouterOutlet } from "@ionic/react";
+import { IonReactRouter } from "@ionic/react-router";
+import { Route } from "react-router-dom";
+import Landing from "./views/landing/Landing.js";
+import Home from "./views/home/Home.js";
 
-function App() {
-  return (
-    <div className="App">
-          Cliente iniciado
-    </div>
-  );
-}
+const App = () => (
+  <IonApp>
+    <IonReactRouter>
+      <IonRouterOutlet>
+        <Route exact path="/" component={Landing} />
+        <Route path="/home" component={Home} />
+      </IonRouterOutlet>
+    </IonReactRouter>
+  </IonApp>
+);
 
 export default App;
