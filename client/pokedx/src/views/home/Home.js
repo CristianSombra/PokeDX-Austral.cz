160675a8dd07170ebb56a6c381e5b6b5a0c5eb78
CristianLegion <108194989+CristianSombra@users.noreply.github.com>
2023-12-13 20:58:09 -0300

    Home, Card, CardsContainer.
    - Home: Se agrega funcionalidad para que al hacer click en el logo de la Nav actualice la página a 1.
    - Card: Se quita la palabra Height ya que no corresponde. Se agrega imagen decorativa.
    - CardsContainer: Se conecta funcionalidad con Home para ejejutar el retorno a la página 1 al hacer click en el logo de la Nav. Se agregan botones para manejo del paginado y descripción de la página actual y el total de páginas.

--- client/pokedx/src/views/home/Home.js
+++ client/pokedx/src/views/home/Home.js
@@ -1,36 +1,40 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import { IonPage, IonHeader, IonToolbar, IonButtons, IonButton, IonContent, IonImg, IonIcon, IonGrid, IonRow } from '@ionic/react';
 import { exitOutline } from 'ionicons/icons';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 import CardsContainer from "../../components/cardsContainer/cardsContainer.js"
 import logo from "../../assets/logo.png";
 
 const Home = () => {
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const handleLogoClick = () => {
+        setCurrentPage(1);
+    }
+
     return (
     <IonPage>
         <IonHeader>
             <IonToolbar color="warning">
                 <IonButtons slot="start">
-                    <IonButton>
-                    <Link to="/home">
+                    <IonButton onClick={handleLogoClick}> 
                         <IonImg src={logo} style={{ width: '55px', height: '55px' }} />
-                    </Link>
                     </IonButton>
                 </IonButtons>
                 <IonButtons slot="end">
-                            <IonButton>
-                                <Link to="/">
-                                <IonIcon icon={exitOutline} style={{ fontSize: '35px', color: 'blue' }} />
-                                </Link>
-                            </IonButton>
-                        </IonButtons>
+                    <IonButton>
+                        <Link to="/">
+                        <IonIcon icon={exitOutline} style={{ fontSize: '35px', color: 'blue' }} />
+                        </Link>
+                    </IonButton>
+                </IonButtons>
             </IonToolbar>
 
             </IonHeader>
             <IonContent>
                 <IonGrid>
                     <IonRow>
-                        <CardsContainer/>
+                        <CardsContainer currentPage={currentPage} setCurrentPage={setCurrentPage}/>
                     </IonRow>
                 </IonGrid>
             </IonContent>
