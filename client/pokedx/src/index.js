6f52345d20f5ad9271d0f6c8fd72ec010cb6ead0
Cristian Sombra <cristiansombra87@gmail.com>
2023-12-12 23:02:07 -0300

    Index, Home, Card
    - Index: Se soluciona error reflejado en consola de navegador.
    - Home: Se modifica para utilizar estilos ionic.
    - Card: Se modifica para utilizar estilos ionic.

--- client/pokedx/src/index.js
+++ client/pokedx/src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './components/redux/store';
 import App from './App';
 
-ReactDOM.render(
-      <Provider store={store}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </Provider>,
-  document.getElementById('root')
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 );
--- client/pokedx/src/views/home/Home.js
+++ client/pokedx/src/views/home/Home.js
@@ -1,14 +1,22 @@
 import React from "react";
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import CardsContainer from "../../components/cardsContainer/cardsContainer";
 
 
-const Home = () => {
+function Home() {
     return (
-        <div>
-        <p>Prueba de Home</p>
-            <CardsContainer/>
-        </div>
-    )
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Home</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+        <IonContent className="ion-padding">
+                <p>Prueba de Home</p>
+                <CardsContainer />
+        </IonContent>
+        </IonPage>
+    );
 }
 
 export default Home;
